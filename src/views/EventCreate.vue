ead99862c0e11e9a68d0303f79116827ee2123c1
Pardip Bhatti <contact@gpcoders.com>
2020-09-21 13:40:27 +0530

    state changes

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Create Event {{ user.name }} </h1>
+    <p>Create Event id: {{ id }} </p>
+    <p>Length: {{ catLength }} </p>
+    <p>{{ getEventById(1) }}</p>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapState} from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapGetters(['getEventById']),
+    ...mapState({
+      user: 'user',
+      id: state => state.user.id
+    }),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
